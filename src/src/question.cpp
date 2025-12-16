380a9b628c12fda3d416ae85c8e02476b7ab1f97
MeryOuma-inf <louizmeriem1@gmail.com>
2025-12-16 16:33:04 +0100

    Update question.cpp

--- src/src/question.cpp
+++ src/src/question.cpp
@@ -1,2 +1,21 @@
-#include "question.h"
+#define QUESTION_H
+
+#include <string>
+
+class question {
+private:
+    std::string d_intitule;
+
+public:
+    question(const std::string& intitule);
+    virtual ~question() = default;
+    std::string Intitule() const;
+    
+    virtual bool estBonneReponse(const std::string& reponse) const =0;
+    virtual std::string getReponseCorrecte() const =0;
+};
+
+#endif
+
+
 
