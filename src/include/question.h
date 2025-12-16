fece1c117d3fc23b7349538db1014afce31716c2
MeryOuma-inf <louizmeriem1@gmail.com>
2025-12-16 16:24:21 +0100

    Update question.h

--- src/include/question.h
+++ src/include/question.h
@@ -5,16 +5,15 @@
 class question {
 private:
     std::string d_intitule;
-    std::string d_texte;
+    
 
 public:
-    question(const std::string& intitule, const std::string& texte);
+    question(const std::string& intitule);
     virtual ~question() = default;
     std::string Intitule() const;
-    std::string Texte() const;
     virtual bool estBonneReponse(const std::string& reponse) const =0;
     virtual std::string getReponseCorrecte() const =0;
 };
 
 #endif
->>>>>>> feature/classes-questions
+
