2816b0cf80c49803ff9e787030fa9d413f1ea2fb
Lilia <liliakernou06@gmail.com>
2025-12-16 08:41:46 +0100

    modification de la classe evaluationTest.cpp

--- src/src/evaluationTest.cpp
+++ src/src/evaluationTest.cpp
@@ -1,12 +1,45 @@
-
 #include "evaluationTest.h"
 
+
 evaluationTest::evaluationTest()
+    : nombreQuestions(0), indiceCourant(0)
 {
-    //ctor
 }
 
-evaluationTest()::~evaluationTest()
-{
-    //dtor
+/*
+ Initialisation :
+ on démarre à la première question
+*/
+void evaluationTest::init(int nQuestions) {
+    nombreQuestions = nQuestions;
+    indiceCourant = 0;
+}
+
+/*
+ Il reste des questions tant que
+ on n'a pas tout parcouru
+*/
+bool evaluationTest::aDesQuestions() const {
+    return indiceCourant < nombreQuestions;
+}
+
+/*
+ Retourne l'indice de la question suivante
+ (dans l'ordre)
+*/
+int evaluationTest::questionSuivante() {
+    return indiceCourant++;
+}
+
+/*
+ La stratégie test ne fait rien
+ après une réponse
+*/
+void evaluationTest::soumettreReponse(int, bool) {
+    // rien à faire
+}
+
+
+bool evaluationTest::peutAfficherCorrection(int) const {
+    return false;
 }
