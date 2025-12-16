70fd720e9e182f7980c0895b35afa06b34d0cb4f
Mokraneyasmine <hechicheyasmine03@gmail.com>
2025-12-16 17:26:28 +0100

    update the class again

--- src/src/apprentissage.cpp
+++ src/src/apprentissage.cpp
@@ -1,24 +1,21 @@
 #include "apprentissage.h"
 #include <conio.h>
 
-apprentissage::apprentissage(const questionnaire& questionnaire)
-     : d_questionnaire(questionnaire)
+apprentissage::apprentissage(const questionaire& questionaire): d_questionaire(questionaire)
 {}
 
 void apprentissage::apprendre() const {
 
-    for (const auto& q : d_questionnaire){
+    questionairIeIterator iterateur{d_questionaire};
+    while (iterateur.hasNext()) {
+        const question& q = iterateur.next();
 
-        d_affichage.clearCMD();
+        d_affichage.nettoyerCMD();
         d_affichage.afficherTitre("Vous etes dans le mode apprentissage");
 
-        d_affichage.afficherQuestion(q->Intitule());
-        d_affichage.afficherReponse(q->getReponseCorrecte());
+        d_affichage.afficherQuestion(q.Intitule());
+        d_affichage.afficherReponse(q.getReponseCorrecte());
 
         d_affichage.afficherMessage("Appuyez sur une touche pour voir la prochaine");
-        getch();
-    }
+        getch(); }
 }
-
-
-
