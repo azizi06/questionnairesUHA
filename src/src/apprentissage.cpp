bab393cfbe0f7d88584cd924df2cf75aebbe679a
Mokraneyasmine <hechicheyasmine03@gmail.com>
2025-12-17 10:52:51 +0100

    correction of a typo in the class apprentissage again

--- src/src/apprentissage.cpp
+++ src/src/apprentissage.cpp
@@ -7,13 +7,13 @@ apprentissage::apprentissage(const questionaire& questionaire): d_questionaire(q
 void apprentissage::apprendre() const {
 
     questionairIeIterator iterateur{d_questionaire};
-    while (iterateur.hasNext()) { //Methode qui sera definie dans le code de Azizi
-        const question& q = iterateur.next(); //Methode qui sera definie dans le code de Azizi
+      while (iterateur.hasNext()) { //Methode qui sera definie dans le code de Azizi
+         const question& q = iterateur.next(); //Methode qui sera definie dans le code de Azizi
 
-        d_affichage.clearCMD();
-        d_affichage.afficherTitre("Vous etes dans le mode apprentissage");
-        d_affichage.afficherQuestion(q.Intitule());
-        d_affichage.afficherReponse(q.getReponseCorrecte());
-        d_affichage.afficherMessage("Appuyez sur une touche pour voir la prochaine");
-        getch(); }
+         d_affichage.clearCMD();
+         d_affichage.afficherTitre("Vous etes dans le mode apprentissage");
+         d_affichage.afficherQuestion(q.Intitule());
+         d_affichage.afficherReponse(q.getReponseCorrecte());
+         d_affichage.afficherMessage("Appuyez sur une touche pour voir la prochaine");
+         getch(); }
 }
