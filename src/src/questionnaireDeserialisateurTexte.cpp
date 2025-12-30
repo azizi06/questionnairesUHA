ea82c4049bb8ed6950a1adab6540a80ef940c9d8
AZIZImohammed <artazizi06@gmail.com>
2025-12-30 13:28:19 +0100

    implementation fonction lirequestionNumerique et lireQuestionTexte + tests unitaires

--- src/src/questionnaireDeserialisateurTexte.cpp
+++ src/src/questionnaireDeserialisateurTexte.cpp
@@ -1,4 +1,5 @@
 #include "../include/questionnaireDeserialisateurTexte.h"
+#include<iostream>
 questionnaireDeserialisateurTexte::questionnaireDeserialisateurTexte(std::istream &ifs):ifs{ifs}{
 
 }
@@ -6,15 +7,54 @@ questionnaireDeserialisateurTexte::questionnaireDeserialisateurTexte(std::istrea
 questionnaire questionnaireDeserialisateurTexte::lire(){
     questionnaire quest{};
 }
+
 questionTexte questionnaireDeserialisateurTexte::lireQuestionTexte(){
+    std::string ligne;
+    //lire{
+    if (!std::getline(ifs, ligne)) {
+        std::cout << "Erreur de lecture: ligne attendue pour question texte";
+    }
+    size_t pos = ligne.find('{');
+    if (pos == std::string::npos) {
+         std::cout << "Format invalide";
+    }
+    std::string enonce = lireString();
+    std::string reponseCorrecte = lireString();
+    //lire }
+  if (!std::getline(ifs, ligne)) {
+        std::cout << "Erreur de lecture: ligne attendue pour question texte";
+    }
+    return questionTexte(enonce, reponseCorrecte);
+
 
 }
 questionNumerique questionnaireDeserialisateurTexte::lireQuestionNumerique(){
+    std::string ligne;
+    //lire{
+    if (!std::getline(ifs, ligne)) {
+        std::cout << "Erreur de lecture: ligne attendue pour question";
+
+    }
+    size_t pos = ligne.find('{');
+    if (pos == std::string::npos) {
+        std::cout << "Format invalide pour question texte";
+    }
+    std::string enonce = lireString();
+    int reponse = std::stoi(lireString());
+    int valeurMin = std::stoi(lireString());
+    int valeurMax = std::stoi(lireString());
+    //lire }
+  if (!std::getline(ifs, ligne)) {
+       std::cout << "Erreur de lecture: ligne attendue pour question";
+    }
+    return questionNumerique(enonce, reponse,valeurMin,valeurMax);
 
 }
 questionChoixMultiple questionnaireDeserialisateurTexte::lireQuestionChoixMultiple(){
 
 }
+
+
 
 std::string questionnaireDeserialisateurTexte::lireString() {
     std::string ligne;
--- test/questionnairesDesirialisateurTest.cpp
+++ test/questionnairesDesirialisateurTest.cpp
@@ -1,7 +1,7 @@
 #include"doctest.h"
 #include "../src/include/questionnaireDeserialisateurTexte.h"
 #include <sstream>
-
+#include <stdexcept>
 TEST_CASE("[questionnaireDeserialisateurTexte] methodes de la classe fonctionne"){
     SUBCASE("lirestring() foctionne"){
         std::string input = "[Titre du questionnaire]";
@@ -20,10 +20,30 @@ TEST_CASE("[questionnaireDeserialisateurTexte] methodes de la classe fonctionne"
 }
 TEST_CASE("questions sont lit correctement"){
     SUBCASE("question text est lit correctement"){
+        std::string input = "{\n[Quelle est la capitale de la France?]\n[Paris]\n}\n";
+        std::istringstream iss(input);
+        questionnaireDeserialisateurTexte deserialisateur(iss);
+        questionTexte q = deserialisateur.lireQuestionTexte();
+        REQUIRE_EQ(q.Intitule(),"Quelle est la capitale de la France?");
+        REQUIRE_EQ(q.getReponseCorrecte(),"Paris");
 
     }
+    SUBCASE("question text est lit corrextement avec des espaces"){
+        std::string input = "{\n[  Question avec des espaces  ]\n[  Réponse avec espaces  ]\n}\n";
+        std::istringstream iss(input);
+        questionnaireDeserialisateurTexte deserialisateur(iss);
+        questionTexte q = deserialisateur.lireQuestionTexte();
+        REQUIRE_EQ(q.Intitule(),"  Question avec des espaces  ");
+        REQUIRE_EQ(q.getReponseCorrecte(),"  Réponse avec espaces  ");
+    }
     SUBCASE("question numérique est lit correctement"){
+        std::string input = "{\n[c'est quoi la temperature moyenne de mois decembre en france?]\n[12]\n[10]\n[14]\n}\n";
+        std::istringstream iss(input);
+        questionnaireDeserialisateurTexte deserialisateur(iss);
+        questionNumerique q = deserialisateur.lireQuestionNumerique();
 
+        REQUIRE_EQ(q.Intitule(),"c'est quoi la temperature moyenne de mois decembre en france?");
+        REQUIRE_EQ(q.getReponseCorrecte(), "12");
     }
     SUBCASE("question choix multiple est lit correctement"){
 
