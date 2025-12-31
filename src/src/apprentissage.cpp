d64ca26a42decf1abbca6e91241961bfc6c3f87b
Lilia <liliakernou06@gmail.com>
2025-12-31 01:31:51 +0100

    modification de apprentissage.cpp

--- src/src/apprentissage.cpp
+++ src/src/apprentissage.cpp
@@ -1,21 +1,33 @@
 #include "../include/apprentissage.h"
-#include <conio.h>
-#include"../include/questionnaireIterator.h"
+#include "../include/questionnaireIterator.h"
+#include <iostream>
+#include <cstdlib> // Pour system("pause")
 
+using namespace std;
 
-apprentissage::apprentissage(const questionnaire& questionnaire): d_questionnaire(questionnaire)
+apprentissage::apprentissage(const questionnaire& questionnaire)
+    : d_questionnaire(questionnaire)
 {}
 
 void apprentissage::apprendre() const {
-
     questionnaireIterator iterateur{d_questionnaire};
-      while (iterateur.hasNext()) { //Methode qui sera definie dans le code de Azizi
-         const question* q = iterateur.next(); //Methode qui sera definie dans le code de Azizi
 
-         d_affichage.clearCMD();
-         d_affichage.afficherTitre("Vous etes dans le mode apprentissage");
-         d_affichage.afficherQuestion(q->Intitule());
-         d_affichage.afficherReponse(q->getReponseCorrecte());
-         d_affichage.afficherMessage("Appuyez sur une touche pour voir la prochaine");
-         getch(); }
+    while (iterateur.hasNext()) {
+        const question* q = iterateur.next();
+
+
+        d_affichage.clearCMD();
+        d_affichage.dessinerCadre();
+        d_affichage.afficherTitre("MODE APPRENTISSAGE");
+
+
+        d_affichage.afficherQuestion(q->Intitule());
+
+
+        d_affichage.afficherReponse(q->getReponseCorrecte());
+
+        d_affichage.afficherMessage("Appuyez sur une touche pour continuer...");
+
+        system("pause");
+    }
 }
